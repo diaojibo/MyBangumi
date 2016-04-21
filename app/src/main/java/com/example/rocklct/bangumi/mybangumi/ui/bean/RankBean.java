34cc028e4b114222556e959e7e848db88235ef17
diaojibo <522250848@qq.com>
2016-04-21 23:11:14 +0800

    a new commit

---
+++ app/src/main/java/com/example/rocklct/bangumi/mybangumi/ui/bean/RankBean.java
@@ -0,0 +1,9 @@
+package com.example.rocklct.bangumi.mybangumi.ui.bean;
+
+/**
+ * Created by Administrator on 2016/4/21.
+ */
+public class RankBean extends BaseBean {
+    int rank;
+    String subject_id;
+}
--- app/src/main/java/com/example/rocklct/bangumi/mybangumi/util/HttpManager.java
+++ app/src/main/java/com/example/rocklct/bangumi/mybangumi/util/HttpManager.java
@@ -2,7 +2,14 @@ package com.example.rocklct.bangumi.mybangumi.util;
 
 import android.os.Handler;
 import android.os.Message;
+import android.provider.DocumentsContract;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -14,32 +21,48 @@ public class HttpManager {
     private String access_token;
     private final String apikey = "";
     private static final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36";
-    public HttpManager(OnConnectListener onConnectListener){
+
+    public HttpManager(OnConnectListener onConnectListener) {
         this.onConnectListener = onConnectListener;
     }
 
-    Handler handler = new Handler(){
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             String result = msg.getData().getString("result");
             int tag = msg.what;
-            if(result == null || result.isEmpty()){
+            if (result == null || result.isEmpty()) {
                 onConnectListener.OnError(tag);
-            }else {
-                onConnectListener.OnSuccess(result,msg.what);
+            } else {
+                onConnectListener.OnSuccess(result, msg.what);
             }
 
         }
     };
 
-    public interface OnConnectListener{
-        void OnSuccess(String result,int tag);
+    public interface OnConnectListener {
+        void OnSuccess(String result, int tag);
+
         void OnError(int tag);
     }
 
-    private List getTopItem(String url,int page){
+    private List getTopItem(String url, int page) {
+        url = url + "&page=" + page;
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element section = doc.getElementById("browserItemList");
+            Elements lists = section.getElementsByTag("li");
+            int num = 0;
+            for (Element li : lists) {
+                num++;
+                int rank = num + (page - 1) * 24;
+                String item = (li.attr("id").split("_"))[1];
+            }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
