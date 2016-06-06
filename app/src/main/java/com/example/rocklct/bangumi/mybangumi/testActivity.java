d748ce347ddcedcc164d22bb32ed9f61bae37ac6
diaojibo <522250848@qq.com>
2016-06-06 21:27:01 +0800

    new commit

--- app/src/main/java/com/example/rocklct/bangumi/mybangumi/testActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.rocklct.bangumi.mybangumi;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Administrator on 2016/3/22.
- */
-public class testActivity extends AppCompatActivity {
-}
